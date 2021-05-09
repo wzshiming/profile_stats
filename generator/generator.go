0b33ad380c9a9393b0c3a073891e44c94bf8cb9d
Shiming Zhang <wzshiming@foxmail.com>
2021-05-09 21:51:15 +0800

    Support fixed blank and show error time

--- generator/generator.go
+++ generator/generator.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/wzshiming/profile_stats"
 	"github.com/wzshiming/profile_stats/generator/activities"
@@ -15,8 +16,9 @@ import (
 	"github.com/wzshiming/xmlinjector"
 )
 
-var (
-	key = []byte("PROFILE_STATS")
+const (
+	key       = "PROFILE_STATS"
+	blankChar = "\n"
 )
 
 type Handler struct {
@@ -42,28 +44,47 @@ func (r *Handler) register(name string, generator profile_stats.Generator) {
 
 func (r *Handler) Handle(ctx context.Context, origin []byte) ([]byte, error) {
 	buf := bytes.NewBuffer(nil)
-	return xmlinjector.Inject(key, origin, func(args, origin []byte) []byte {
+	return xmlinjector.Inject([]byte(key), origin, func(args, origin []byte) []byte {
 		tag := NewArgs(reflect.StructTag(args))
 		template, ok := tag.String("template")
 		if !ok || template == "" {
-			return errInfo("no template")
+			return errInfo("no template", origin)
+		}
+
+		blank, ok := tag.Int("blank")
+		if !ok {
+			blank = 2
 		}
 
 		generator, ok := r.registry[template]
 		if !ok {
-			return errInfo(fmt.Sprintf("not support template %q", template))
+			return errInfo(fmt.Sprintf("not support template %q", template), origin)
 		}
 		buf.Reset()
-		buf.Write([]byte("\n"))
 		err := generator.Generate(ctx, buf, tag)
 		if err != nil {
-			return errInfo(err.Error())
+			return errInfo(err.Error(), origin)
 		}
-		buf.Write([]byte("\n"))
-		return buf.Bytes()
+
+		raw := buf.Bytes()
+		raw = bytes.Trim(raw, blankChar)
+
+		var tmp []byte
+
+		if blank > 0 {
+			tmp = make([]byte, 0, len(raw)+blank*2)
+			blanks := bytes.Repeat([]byte(blankChar), blank)
+			tmp = append(tmp, blanks...)
+			tmp = append(tmp, raw...)
+			tmp = append(tmp, blanks...)
+		} else {
+			tmp = make([]byte, len(raw))
+			copy(tmp, raw)
+		}
+		return tmp
 	})
 }
 
-func errInfo(msg string) []byte {
-	return []byte(fmt.Sprintf("\n<!-- error: %q -->\n", msg))
+func errInfo(msg string, origin []byte) []byte {
+	return append([]byte(fmt.Sprintf("\n<!-- profile_stats_error error:%q date:%q /-->\n", msg, time.Now().Format(time.RFC3339))), origin...)
 }
